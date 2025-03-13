3f76ea78b9307712cc8ae358d2ce3c9867060725
Sepehr Sadeghpour <sepehr.sad78@gmail.com>
2025-03-13 21:42:26 +0330

    🔧 Setup Git configuration

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+    plugins: [
+
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        vueDevTools({
+            appendTo: 'resources/js/app.ts',
+            launchEditor : "phpstorm"
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
+    },
+});
